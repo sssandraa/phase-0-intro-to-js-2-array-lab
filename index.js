872a8ee51fdbda2d0e899504c50d984d0fd94411
Sandra Wazaz <sandrawazaz@gmail.com>
2021-06-29 18:52:43 -0400

    commit tree d051eb30e0eb2bf22b3f95b4a07f2c14345833c6

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution here!
+
+const cats =[]
+
+cats.push("Milo", "Otis")
+function appendKitten(name){
+    var newArray = kittens.slice();
+    // or ES6 way
+    // var newArray = [...kittens];
+    newArray.push(name)
+    return newArray
+  }
+  
